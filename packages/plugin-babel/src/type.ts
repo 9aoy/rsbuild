fcc19e52cf3599f4d3d155534778cc608f477a30
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2023-10-27 14:29:42 +0800

    fix: update babel doc entry

--- packages/plugin-babel/src/type.ts
+++ packages/plugin-babel/src/type.ts
@@ -4,6 +4,15 @@ import type {
   BabelConfigUtils,
 } from '@rsbuild/shared';
 
+/**
+ * Modify the options of [babel-loader](https://github.com/babel/babel-loader)
+ *
+ * When `babelOptions`'s type is Function，the default babel config will be passed in as the first parameter, the config object can be modified directly, or a value can be returned as the final result.
+ *
+ * When `babelOptions`'s type is `Object`, the config will be shallow merged with default config by `Object.assign`.
+ *
+ * Note that `Object.assign` is a shallow copy and will completely overwrite the built-in `presets` or `plugins` array, please use it with caution.
+ */
 export type PluginBabelOptions = ChainedConfig<
   BabelTransformOptions,
   BabelConfigUtils
